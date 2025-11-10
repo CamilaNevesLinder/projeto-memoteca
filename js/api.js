d89dda6af76103a803bbe037ceb8ae2c7cdd79d2
CamilaNevesLinder <camilaneveslindner@.com>
2025-11-10 13:12:36 -0300

    refactor:add buttons

--- js/api.js
+++ js/api.js
@@ -1,7 +1,9 @@
+const URL_BASE = "http://localhost:3000";
+
 const api = {
   async buscarPensamentos() {
     try {
-      const response = await fetch("http://localhost:3000/pensamentos");
+      const response = await fetch(`${URL_BASE}/pensamentos`);
       return await response.json();
     } catch (error) {
       alert("Erro ao buscar pensamentos");
@@ -11,16 +13,51 @@ const api = {
 
   async salvarPensamento(pensamento) {
     try {
-      const response = await fetch("http://localhost:3000/pensamentos", {
+      const response = await fetch(`${URL_BASE}/pensamentos`, {
         method: "POST",
         headers: {
-          "Content-type": "aplication/json",
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(pensamento),
       });
       return await response.json();
     } catch (error) {
       alert("Erro ao salvar pensamentos");
+      throw error;
+    }
+  },
+  async buscarPensamentoPorId(id) {
+    try {
+      const response = await fetch(`${URL_BASE}/pensamentos/${id}`);
+      return await response.json();
+    } catch (error) {
+      alert("Erro ao buscar pensamento");
+      throw error;
+    }
+  },
+
+  async editarPensamento(pensamento) {
+    try {
+      const response = await fetch(`${URL_BASE}/pensamentos/${pensamento.id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(pensamento),
+      });
+      return await response.json();
+    } catch (error) {
+      alert("Erro ao editar pensamentos");
+      throw error;
+    }
+  },
+  async excluirPensamento(id) {
+    try {
+      const response = await fetch(`${URL_BASE}/pensamentos/${id}`, {
+        method: "DELETE",
+      });
+    } catch (error) {
+      alert("Erro ao excluir pensamentos");
       throw error;
     }
   },
--- js/main.js
+++ js/main.js
@@ -18,9 +18,13 @@ async function manipularSubmissaoFormulario(event) {
   const autoria = document.getElementById("pensamento-autoria").value;
 
   try {
-    await api.salvarPensamento({ conteudo, autoria });
+    if (id) {
+      await api.editarPensamento({ id, conteudo, autoria });
+    } else {
+      await api.salvarPensamento({ conteudo, autoria });
+    }
     ui.renderizarPensamentos();
-  } catch {
+  } catch (error) {
     alert("Erro ao salvar pensamento");
   }
 }
